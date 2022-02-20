74090d9aece4d1b325554b769d7d074cd2d689b3
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-02-19 23:16:29 -0300

    added typescript and settings

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
 import { Negociacao } from "./models/negociacao.js";
+
 const negociacao = new Negociacao(new Date(), 50, 105);
 console.log(negociacao);
-console.log('Volume ' + negociacao.volume);
---
+++ app/models/negociacao.ts
@@ -0,0 +1,27 @@
+export class Negociacao {
+  private _data;
+  private _valor;
+  private _quantidade;
+
+  constructor(data, valor, quantidade) {
+    this._data = data;
+    this._valor = valor;
+    this.+quantidade = quantidade;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  get valor() {
+    return this._valor;
+  }
+
+  get quantidade() {
+    return this._quantidade;
+  }
+
+  get volume() {
+    return this._quantidade * this._valor;
+  }
+}
