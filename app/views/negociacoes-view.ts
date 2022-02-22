0071726cc323a80169bb855af02abccaf86a8b88
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-02-22 10:11:23 -0300

    some changes

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -21,7 +21,7 @@ export class NegociacoesView extends View<Negociacoes>{
                              <td>${negociacao.quantidade}</td>
                              <td>${negociacao.valor}</td>                       
                         </tr>
-                    `;     
+                    `;    
                 }).join('')}
             </tbody>
         </table> 
